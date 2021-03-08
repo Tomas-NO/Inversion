c2c10e4b71c4eb7eceba7fef578aa6b471dafd6c
Tomas Olivera <tomasnolivera@gmail.com>
2021-03-08 17:21:01 -0300

    Segunda subida

--- JS/main.js
+++ JS/main.js
@@ -34,25 +34,4 @@ $(document).ready(function() {
             console.log(status)
             console.log(error)
         })
-
-
 })
-
-
-//let activos = JSON.parse(activosJSON).activos
-// crear_activos()
-
-// // Aplico el desplazamiento de los links del header
-// scroll_inicio()
-
-// // Traer diferente info dependiendo del activo del que se quiera saber mas
-// $('.boton-conoce-mas').click(crear_conoce_mas)
-
-// // Evento para abrir el resumen
-// $('#abrir-resumen').click(abrir_resumen)
-
-// // Asigna el evento para filtrar los activos
-// $('.filtradores').click(filtrar_activos)
-
-// // Muestra o esconde el contenido de las preguntas frecuentes
-// $('.pregunta').click(ver_pregunta)
